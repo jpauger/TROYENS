45aa17c8e68a0b3873b0bb411e26e8d2162b8e24
jpauger <jp.auger88@gmail.com>
2014-12-14 23:59:50 -0500

    fix supression

--- Recyclapp/src/domaine/PlanDeTravail.java
+++ Recyclapp/src/domaine/PlanDeTravail.java
@@ -48,21 +48,24 @@ public class PlanDeTravail implements java.io.Serializable {
     public void supprimerStation (int equip)
     {
         Equipement equipement = listeEquipement.get(equip);
-        ArrayList<SortieEquipement> sortieASupprime = equipement.listeSorties;
         System.out.println("On supprime "+equipement.nom);
-        for(int j=0; j<sortieASupprime.size(); j++)
+        
+        for(int i =0; i<listeConvoyeur.size();i++)
         {
-            for(int i=0; i<listeConvoyeur.size();i++)
-             {
-                 if(listeConvoyeur.get(i).equipement == equipement || listeConvoyeur.get(i).sortie == sortieASupprime.get(j) )      
-                 {
-                     //libérer la sortie qui de la station
-                     listeConvoyeur.remove(i);
-                     listeConvoyeur.remove(j);
-                 }
-             }
-            listeEquipement.remove(equipement);
+            if(listeConvoyeur.get(i).equipement == equipement)
+                listeConvoyeur.remove(i);
         }
+        
+        for(int i=0; i<listeConvoyeur.size();i++)
+         {
+             if(listeConvoyeur.get(i).sortie.equipementMere() == equipement)      
+             {
+                 //libérer la sortie qui de la station
+                 listeConvoyeur.remove(i);
+                 i--;
+             }
+         }
+        listeEquipement.remove(equipement);
     }
     //deplace une station prise en paramettre vers de nouvelles coordonnées prise en parametre
     public void relocaliserStation (Equipement unEquipement, Coordonnee coorArrivee)
